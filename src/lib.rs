7f63da6aeee8fc7e24b9f2176508ef4b6b2ed3e8
hearsay316 <hearsay316@gmail.com>
2024-09-17 11:53:11 +0800

    文档更新: 为`EnumFrom`过程宏添加注释
    为`EnumFrom`过程宏的实现添加了详细的注释，以帮助开发人员更好地理解其工作原理。该宏为Rust中的枚举类型自动生成`From`实现。注释解释了如何将输入的`TokenStream`解析为`syn::DeriveInput`，如何检查输入是否为枚举类型，以及如何为具有单个未命名字段的枚举变体生成`From`实现。

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,35 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::Fields;
-
+/// 这段代码定义了一个名为 EnumFrom 的过程宏，它用于为 Rust 中的枚举类型自动生成 From 实现。过程宏是一种在编译时运行的宏，它接收 Rust 代码作为输入，然后生成新的 Rust 代码作为输出。
+// 首先，宏通过 #[proc_macro_derive(EnumFrom)] 属性声明为过程宏，并定义了一个名为 derive_enum_from 的函数，该函数接收一个 TokenStream 类型的参数，这是 Rust 编译器内部表示代码的一种方式。
+// 在函数内部，使用 syn::parse_macro_input! 宏将输入的 TokenStream 解析为一个 syn::DeriveInput 结构体，这个结构体包含了关于输入代码的信息，比如标识符、数据等。
+// 接下来，代码获取了枚举类型的标识符，并检查输入的数据是否为枚举类型。如果不是枚举类型，宏会触发一个 panic，提示 EnumFrom 只能在枚举上使用。
+// 如果输入是枚举类型，代码会获取枚举的所有变体，并遍历这些变体。对于每个变体，它会检查变体的字段是否未命名且数量为 1。如果条件满足，它会生成一个 From 实现，允许从这个字段的类型转换到枚举类型。
+// 最后，所有生成的 From 实现会被合并，并转换回 TokenStream 类型，然后返回给编译器。这样，当用户在他们的代码中使用 #[derive(EnumFrom)] 属性时，编译器会调用这个过程宏，并将生成的 From 实现插入到用户的代码中。
+// 这个宏的一个关键点是它只处理未命名字段的变体，并且这些字段的数量必须为 1。这是因为 From 实现需要一个单一的值来构造枚举变体。如果变体有命名字段或多个未命名字段，宏将不会生成 From 实现。
+// 这个宏的使用可以大大减少为枚举类型手动编写 From 实现的重复工作，特别是在处理大量变体时，可以提高代码的可维护性和开发效率。
 // proc  macro crate
 // for  enum .we'd like to generate From impls for each variant.
-#[proc_macro_derive(EnumFrom)]
+#[proc_macro_derive(EnumFrom)] // 定义一个过程宏 EnumFrom
 pub fn derive_enum_from(input: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(input as syn::DeriveInput);
- // let input = syn::parse_macro_input!(input as syn::DeriveInput);
- //    println!("{:#?}",input);
-    // input
-    // 取 ident
-    let ident = input.ident;
-    let variants = match input.data{
-        syn::Data::Enum(data)=>data.variants,
-        _=>panic!("EnumFrom only works on enums "),
+    let input = syn::parse_macro_input!(input as syn::DeriveInput); // 解析输入的TokenStream为DeriveInput结构体
+    let ident = input.ident; // 获取枚举的标识符
+    let variants = match input.data{ // 匹配输入数据为枚举类型
+        syn::Data::Enum(data)=>data.variants, // 获取枚举的所有变体
+        _=>panic!("EnumFrom only works on enums "), // 如果不是枚举类型则报错
     };
-    let from_impls = variants.iter().map(
+    let from_impls = variants.iter().map( // 遍历每个变体并生成From实现
         |variant|{
-            let var = &variant.ident;
-            match &variant.fields {
-                Fields::Unnamed(fields)=>{
-                    if fields.unnamed.len()!=1{
+            let var = &variant.ident; // 获取变体的标识符
+            match &variant.fields { // 匹配变体的字段
+                Fields::Unnamed(fields)=>{ // 如果字段未命名
+                    if fields.unnamed.len()!=1{ // 如果未命名字段的数量不为1，则不生成实现
                         quote!{}
-                    }else {
-                        let field = fields.unnamed.first().expect("应该只有一个");
-                        let ty = &field.ty;
-                        quote! {
+                    }else { // 如果只有一个未命名字段
+                        let field = fields.unnamed.first().expect("应该只有一个"); // 获取第一个字段
+                        let ty = &field.ty; // 获取字段类型
+                        quote! { // 生成From实现
                             impl From<#ty> for #ident{
                                 fn from(v:#ty)-> Self{
                                     #ident::#var(v)
@@ -35,14 +38,14 @@ pub fn derive_enum_from(input: TokenStream) -> TokenStream {
                         }
                     }
                 }
-                _=>{
+                _=>{ // 如果字段已命名或其他情况，则不生成实现
                     quote!{}
                 }
             }
         });
-    (quote!{
+    (quote!{ // 将所有生成的From实现合并
         #(#from_impls)*
     })
-    .into()
-
+    .into() // 转换为TokenStream并返回
 }
+
